c34d44abd831f688ae9e1961150583c7dc34d791
mrcliffster <tristan.cliff@hotmail.com>
2019-02-19 19:18:42 -0800

    Initial Commit
    First vcs use

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera room1;
+    public Camera room2;
+    public Camera room3;
+
+    [SerializeField]
+    private float lowerBound;
+    [SerializeField]
+    private float upperBound;
+
+    private Camera activeCam;
+    private CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeCam = room1;
+        activeCam.enabled = true;
+        room2.enabled = false;
+        room3.enabled = false;
+        cc = FindObjectOfType<CharacterController>();
+    }
+    
+    void LateUpdate()
+    {
+        Debug.Log(activeCam);
+        if (activeCam.Equals(room1))
+        {
+            // blank, this camera is static
+        }
+        else if (activeCam.Equals(room2))
+        {
+            // Track with axis
+            Vector3 tempVec3 = new Vector3
+            {
+                z = cc.transform.position.z,
+                y = activeCam.transform.position.y,
+                x = activeCam.transform.position.x
+            };
+            if (tempVec3.z < lowerBound)
+            {
+                tempVec3.z = lowerBound;
+            } 
+            else if (tempVec3.z > upperBound)
+            {
+                tempVec3.z = upperBound;
+            }
+            activeCam.transform.position = tempVec3;
+        }
+        else if (activeCam.Equals(room3))
+        {
+            // LookAt character
+            activeCam.transform.LookAt(cc.transform);
+        }
+    }
+
+    public void EnterRoom1()
+    {
+        activeCam.enabled = false;
+        activeCam = room1;
+        activeCam.enabled = true;
+    }
+
+    public void EnterRoom2()
+    {
+        activeCam.enabled = false;
+        activeCam = room2;
+        activeCam.enabled = true;
+    }
+
+    public void EnterRoom3()
+    {
+        activeCam.enabled = false;
+        activeCam = room3;
+        activeCam.enabled = true;
+    }
+}
---
+++ Assets/Yurowm/Demo/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Animator))]
+public class PlayerController : MonoBehaviour {
+
+	public Transform rightGunBone;
+	public Transform leftGunBone;
+	public Arsenal[] arsenal;
+    public int moveSpeed;
+    public int rotationSpeed;
+
+	private Animator animator;
+    private CharacterController cc;
+    private CameraManager cm;
+    private Actions actions;
+
+    void Awake() {
+		animator = GetComponent<Animator> ();
+        actions = GetComponent<Actions>();
+        cc = GetComponent<CharacterController>();
+        cm = FindObjectOfType<CameraManager>();
+		if (arsenal.Length > 0)
+			SetArsenal (arsenal[0].name);
+		}
+
+	public void SetArsenal(string name) {
+		foreach (Arsenal hand in arsenal) {
+			if (hand.name == name) {
+				if (rightGunBone.childCount > 0)
+					Destroy(rightGunBone.GetChild(0).gameObject);
+				if (leftGunBone.childCount > 0)
+					Destroy(leftGunBone.GetChild(0).gameObject);
+				if (hand.rightGun != null) {
+					GameObject newRightGun = (GameObject) Instantiate(hand.rightGun);
+					newRightGun.transform.parent = rightGunBone;
+					newRightGun.transform.localPosition = Vector3.zero;
+					newRightGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
+					}
+				if (hand.leftGun != null) {
+					GameObject newLeftGun = (GameObject) Instantiate(hand.leftGun);
+					newLeftGun.transform.parent = leftGunBone;
+					newLeftGun.transform.localPosition = Vector3.zero;
+					newLeftGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
+				}
+				animator.runtimeAnimatorController = hand.controller;
+				return;
+				}
+		}
+	}
+
+	[System.Serializable]
+	public struct Arsenal {
+		public string name;
+		public GameObject rightGun;
+		public GameObject leftGun;
+		public RuntimeAnimatorController controller;
+	}
+
+    public void Update()
+    {
+        float fwd = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0));
+        if (System.Math.Abs(fwd) > 0) {
+            cc.Move(transform.TransformDirection(Vector3.forward * Time.deltaTime * moveSpeed * fwd));
+            actions.Run();
+        }
+        else
+        {
+            actions.Stay();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Entered another room");
+        if (other.name.Contains("Room1"))
+        {
+            Debug.Log("Entered room 1");
+            cm.EnterRoom1();
+        } else if (other.name.Contains("Room2"))
+        {
+            Debug.Log("Entered room 2");
+            cm.EnterRoom2();
+        } else if (other.name.Contains("Room3"))
+        {
+            Debug.Log("Entered room 3");
+            cm.EnterRoom3();
+        }
+    }
+}
